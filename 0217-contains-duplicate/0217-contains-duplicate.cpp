e7a775057a321f0271bec8b2f454ccbb4c83f0a7
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-02-16 21:45:44 +0530

    Time: 135 ms (22.35%), Space: 75.9 MB (5.29%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // better approach
+        
+        /* 1. brute force
+        int n = nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+        2. better approach
         sort(nums.begin(),nums.end());
         int n = nums.size();
         for(int i=0;i<n-1;i++){
@@ -10,5 +22,11 @@ public:
                 }
         }
         return false;
+        
+        3. set approach
+        4. optimal but contains tradeoff
+        */
+        return nums.size() > set<int>(nums.begin(),nums.end()).size();
+        
     }
 };
