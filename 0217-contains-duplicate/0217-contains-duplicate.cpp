d8e0db6494d49c05f5f6ee324656c855572e59cb
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-02-16 21:52:44 +0530

    Time: 91 ms (76.38%), Space: 73.1 MB (32.50%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -16,17 +16,25 @@ public:
         2. better approach
         sort(nums.begin(),nums.end());
         int n = nums.size();
-        for(int i=0;i<n-1;i++){
-                if(nums[i]==nums[i+1]){
+        for(int i=1;i<n;i++){
+                if(nums[i]==nums[i-1]){
                     return true;
                 }
         }
         return false;
         
         3. set approach
-        4. optimal but contains tradeoff
-        */
         return nums.size() > set<int>(nums.begin(),nums.end()).size();
         
+        4. optimal but contains tradeoff
+        */
+        unordered_set<int> freq;
+        for (int i: nums){
+            if(freq.count(i)>0){
+                return true;
+            }
+            freq.insert(i);
+        }  
+        return false;
     }
 };
