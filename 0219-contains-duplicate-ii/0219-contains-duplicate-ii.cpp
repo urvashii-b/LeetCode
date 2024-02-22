8d10626ba474f6484a67723ce332ccac6c26be30
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-02-22 09:55:04 +0530

    Time: 126 ms (61.80%), Space: 75.9 MB (85.78%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-    unordered_set<int> window;
+    unordered_set<int> window; // Sliding window approach
     int n = nums.size();
     int l = 0;
     for (int r = 0; r < n; ++r) {
