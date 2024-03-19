16c239bb6f4d4a5dd4fe7d949f34f096161b4024
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-03-19 23:21:13 +0530

    Time: 3 ms (38.59%), Space: 7.3 MB (28.73%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int sum(int num1, int num2) {
         /* 
-        1. BASIC:
-        return num1+num2;
-        
-        2. BITWISE:
-        */
+        1. BITWISE: 
         return (num2==0)? num1: sum(num1^num2,((unsigned int)(num1&num2))<<1);
+        
+        2. BASIC:
+        */
+        return num1+num2;
     }
 };
