22515601505732a5ac91feba77873d04194668e0
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-03-23 10:26:37 +0530

    Time: 6 ms (87.62%), Space: 16 MB (94.14%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,7 +5,7 @@ private:
     void markRow(vector<vector<int>>& matrix, int n, int m, int i){
         for(int j=0; j<m; j++){
             if(matrix[i][j]!=0){
-                matrix[i][j]=-1;
+                matrix[i][j]=-1; // doesn't work here as range of numbers might include -1
             }
         }
     }
@@ -13,7 +13,7 @@ private:
     void markCol(vector<vector<int>>& matrix, int n, int m, int j){
         for(int i=0; i<n; i++){
             if(matrix[i][j]!=0){
-                matrix[i][j]=-1;
+                matrix[i][j]=-1; // doesn't work here as range of numbers might include -1
             }
         }
     }
@@ -41,7 +41,7 @@ public:
 };
 
 2. BETTER
-*/
+
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
@@ -67,4 +67,41 @@ public:
         }
         
     }
+*/
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int col0 =1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0)
+                        matrix[0][j]=0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int j=0;j<m;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col0==0){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }    
 };
