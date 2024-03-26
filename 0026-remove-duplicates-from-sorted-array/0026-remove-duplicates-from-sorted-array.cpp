19f45b8b3709dd1c88e7b13a2e3132f2a392913c
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-03-26 09:45:36 +0530

    Time: 4 ms (85.61%), Space: 21 MB (64.96%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,29 +2,35 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         /* 
-        BRUTE FORCE
+        BRUTE FORCE = O(n*log(n))+O(n)
+        
+        // set data structure as it stores unique elements
         set<int> st;
         for(int i=0;i<nums.size();i++){
             st.insert(nums[i]);
         }
+        int k = st.size();
         int index = 0;
+        // update original array with the non repeating elements present in set
         for (auto it: st){
             nums[index] = it;
             index++;
         }
-        return index;
+        // size of set is the number of unique elements in the array
+        return k;
         
-        OPTIMAL
+        OPTIMAL = 
         */
         int i = 0;
         int n = nums.size();
+        // iterate from j=1 to n and update using i if there is a unique element
         for(int j=1;j<n;j++){
             if(nums[i]!=nums[j]){
-                nums[i+1]=nums[j];
                 i++;
+                nums[i]=nums[j];
             }
         }
+        // as it is 0-based indexing
         return (i+1); 
-        
     }
 };
