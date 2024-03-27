0972eeb7acd8d9e91d3606d0052e036de7b81e9a
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-03-27 21:55:58 +0530

    Time: 3 ms (79.46%), Space: 12.5 MB (91.62%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        /*
+        1. BRUTE
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
         
@@ -18,6 +20,27 @@ public:
                 if(nums2[j]>nums1[i]){
                     break;
                 }
+            }
+        }
+        return ans;
+        
+        2. OPTIMAL
+        */
+        int i=0,j=0, n=nums1.size(), m=nums2.size();
+        vector<int> ans;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        while(i<n && j<m){
+            if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else{
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
             }
         }
         return ans;
