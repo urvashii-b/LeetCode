2cf49d37a15a6f4c78917f2b31cdc7832bd7587b
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-03-28 19:32:53 +0530

    Time: 8 ms (89.69%), Space: 20.5 MB (33.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -39,13 +39,12 @@ public:
         
         4. OPTIMAL2 - XOR
         */
-        
         int n = nums.size();
-        int ans =0;
-        for(int i =1;i<=n;i++){
-            ans = ans ^ i;
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            ans = ans^i;
         }
-        for(int i =0;i<nums.size();i++){
+        for(int i=0;i<=n-1;i++){
             ans = ans^nums[i];
         }
         return ans;
