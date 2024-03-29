4a6a5f85c51f753adec50ccf03ca7fed3de75c40
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-03-29 08:59:47 +0530

    Time: 34 ms (6.98%), Space: 22.9 MB (20.08%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         /*
         1. BRUTE FORCE - O(N^2)
-        */
+        
         int n = nums.size();
         for(int i=0;i<n;i++){
             int sel = nums[i];
@@ -15,6 +15,36 @@ public:
             }
             if(cnt==1){
                 return sel;
+            }
+        }
+        return -1;
+        
+        2. HASHING - O(N)+O(N)+O(N)
+        cannot use if large numbers/ negative numbers
+        
+        int n = nums.size();
+        int hash[30001]={0};
+        for(int i=0;i<n;i++){
+            hash[nums[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            if(hash[nums[i]]==1){
+                return nums[i];
+            }
+        }
+        return -1;
+        
+        3. MAP - O(N*logM) + O(M) where M (size of map) = N/2 + 1
+        */
+        int n = nums.size();
+        // map<long long, int> mpp; ---> depending upon size of elements
+        map<int, int> mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp){
+            if(it.second==1){
+                return it.first;
             }
         }
         return -1;
