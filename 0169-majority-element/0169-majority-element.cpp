c9bc6c65f34fb740e6e2a71113669cbc5ac1e16c
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-03-30 09:43:32 +0530

    Time: 15 ms (55.76%), Space: 27.8 MB (8.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        /*
+        1. MID ELE IN SORTED ARRAY
         sort(nums.begin(),nums.end());
         int n = nums.size();
         return nums[n/2];
+        
+        2.HASHMAP
+        */
+        int n = nums.size();
+        unordered_map<int,int> mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        n=n/2;
+        for(auto it:mpp){
+            if(it.second>n){
+                return it.first;
+            }
+        }
+        return -1;
     }
 };
