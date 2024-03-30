957f92cd36ab453d4f2afbd298a18beb01cc61fe
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-03-30 09:30:38 +0530

    Time: 4 ms (67.43%), Space: 13.4 MB (41.73%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        /*
         int i =0;
         // int ele= nums[0];
         for(auto ele : nums)
@@ -11,6 +12,15 @@ public:
                 i++;
             }
         }
-    return i ;
+        return i ;
+        */
+        int j = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (j == 1 || nums[i] != nums[j - 2]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
     }
 };
