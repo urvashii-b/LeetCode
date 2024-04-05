7b56440824c01881b72db8b967e33e1eb24f5f6a
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-04-05 07:18:32 +0530

    Time: 2 ms (58.69%), Space: 8.6 MB (20.58%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+        for(char c: s){
+            if(!st.empty() && abs(c-st.top())==32){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        string res;
+        while(!st.empty()){
+            res = st.top()+res;
+            st.pop();
+        }
+        return res;
+    }
+};
