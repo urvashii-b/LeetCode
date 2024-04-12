e093f583db026bfd2c2f42126afc1116786cfd29
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-04-12 12:03:25 +0530

    Time: 4 ms (94.43%), Space: 14.2 MB (17.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        /*
-        1. BRUTE FORCE
-        */
-        vector<int> ans;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            int digit = target - nums[i];
+        /* 1. Brute Force
+       
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
-                if(nums[j]==digit){
+                if(nums[i]+nums[j]==target){
                     return {i,j};
                 }
             }
         }
-        return {0,0};
+        return {};
+        
+        2. Better using hashtable - unordered_map (two pass)
+        unordered_map<int,int> freq;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            freq[nums[i]]=i;
+        }
+        for(int i=0;i<n;i++){
+            int complement = target - nums[i];
+            if(freq.count(complement) && freq[complement]!=i){
+                return {i,freq[complement]};
+            }
+        }
+        return {};
+        
+        3. Better using hashtable - unordered_map (one pass)
+        */
+        int n = nums.size();
+        unordered_map<int,int> freq;
+        for(int i=0;i<n;i++){
+            int complement = target - nums[i];
+            if(freq.count(complement)){
+                return {freq[complement],i};
+            }
+            freq[nums[i]]=i;
+        }
+        return {};
     }
 };
