772c4ddd89819de03be81f59fef6df03dcea6235
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-04-25 20:23:32 +0530

    Time: 3 ms (47.11%), Space: 7.6 MB (79.09%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -3,15 +3,21 @@ public:
     string mergeAlternately(string word1, string word2) {
         string res="";
         int i=0;
-        while(i<word1.length() || i<word2.length()){
-            if(i<word1.length()){
-                res+=word1[i];
-            }
-            if(i<word2.length()){
-                res+=word2[i];
-            }
+        int j=0;
+        while(i<word1.length() && j<word2.length()){
+            res+=word1[i];
+            res+=word2[j];
             i++;
-        } 
+            j++;
+        }
+        while(i<word1.length()){
+            res+=word1[i];
+            i++;
+        }
+        while(j<word2.length()){
+            res+=word2[j];
+            j++;
+        }
         return res;
     }
 };
