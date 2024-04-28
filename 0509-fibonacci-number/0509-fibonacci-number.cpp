e99521500450fc316163f11523377db3b1893911
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-04-29 00:41:57 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (34.43%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,27 @@
 class Solution {
 public:
+    static vector<int> dp;
+    Solution(){
+        if(dp.empty()){
+            dp.resize(31,-1);
+        }
+    }
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+        if(n<=1){
+            return n;
+        }
+        int first, second;
+        if(dp[n-1]!=-1){
+            first = dp[n-1];
+        }else{
+            first = fib(n-1);
+        }
+        if(dp[n-2]!=-1){
+            second = dp[n-2];
+        }else{
+            second = fib(n-2);
+        }
+        return dp[n]=first+second;
     }
 };
+vector<int> Solution::dp;
