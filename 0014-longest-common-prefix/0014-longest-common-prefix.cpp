089c083b7e3ebced1111ff214a0d65b3f2b2d62a
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-06-03 11:56:51 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (98.23%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans = "";
-        for(int i=0;i<strs[0].length();i++){
-            char ch = strs[0][i];
-            bool match = true;
-            for(int j=1;j<strs.size();j++){
-                if(strs[j][i]!= ch || strs[j].size()<i){
-                    match = false;
-                    break;
-                }
-            }
-            if(match==false){
-                break;
-            }else{
-                ans.push_back(ch);
+        string res = strs[0];
+        int charIndex = 0;
+        long maxCharIndex = strs[0].length();
+        for (int i = 1; i < strs.size(); ++i) {
+            if (strs[i].length() > maxCharIndex) {
+                maxCharIndex = strs[i].length();
             }
         }
-        return ans;
+        while (charIndex < maxCharIndex) {
+            char prevChar = strs[0][charIndex];
+            for (int i = 1; i < strs.size(); ++i) {
+                if (prevChar == strs[i][charIndex]) {
+                    continue;
+                }
+                return res.substr(0, charIndex);
+            }
+            ++charIndex;
+            res += prevChar;
+        }
+        return res.substr(0, charIndex);
     }
 };
