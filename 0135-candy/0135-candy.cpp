81ca45efbc8cec3ae7b343bd1b6b68523df36b38
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-06-12 16:19:15 +0530

    Time: 12 ms (80.53%), Space: 20.2 MB (36.45%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> arr(n,1);
+        for(int i=1;i<n;i++){ // left to right (compare w left)
+            if(ratings[i]>ratings[i-1]){
+                arr[i]=arr[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                arr[i]=max(arr[i+1]+1,arr[i]);
+            }
+        }
+        int total = 0;
+        for(int i=0;i<arr.size();i++){
+            total+=arr[i];
+        }
+        return total;
+    }
+};
