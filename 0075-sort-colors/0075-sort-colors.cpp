e84f7c96eae8165710d119dca324eec7239517ab
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-06-13 00:04:45 +0530

    Time: 4 ms (31.04%), Space: 9.7 MB (71.79%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int lo = 0, mid = 0, hi = n-1;
+        while(mid<=hi){
+            if(nums[mid]==0){
+                swap(nums[lo],nums[mid]);
+                lo++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums[mid],nums[hi]);
+                hi--;
+            }
+        }
+    }
+};
