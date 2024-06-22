8c5a422600d1292f902e8b26f78fcb61fff524b9
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-06-22 18:13:33 +0530

    Time: 308 ms (5.00%), Space: 109.3 MB (6.11%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -4,16 +4,9 @@ public:
         int n = nums.size(), prefixSum = 0, cnt = 0;
         map<int,int> mpp;
         mpp[0]=1;
-        for(int i=0;i<n;i++){ // converting to binary
-            if(nums[i]%2==0){
-                nums[i]=0;
-            }
-            else{
-                nums[i]=1;
-            }
-        }
+        
         for(int i=0;i<n;i++){
-            prefixSum+=nums[i];
+            prefixSum+=nums[i]%2;
             int rem = prefixSum-k;
             cnt+=mpp[rem];
             mpp[prefixSum]++;
