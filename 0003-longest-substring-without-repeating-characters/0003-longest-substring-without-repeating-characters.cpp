404d94fa3f84e5ca2ae9847fc18f16276f9acc62
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-06-26 15:48:43 +0530

    Time: 4 ms (91.22%), Space: 9.7 MB (90.75%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,17 +4,13 @@ public:
         vector<int> mpp(128,0);
         int counter=0, begin=0, end=0, ans=0;
         while(end<s.size()){
-            if(mpp[s[end]]>0){
+            if(mpp[s[end++]]++>0){
                 counter++;
             }
-            mpp[s[end]]++;
-            end++;
             while(counter>0){
-                if(mpp[s[begin]]>1){
+                if(mpp[s[begin++]]-->1){
                     counter--;
                 }
-                mpp[s[begin]]--;
-                begin++;
             }
             ans = max(ans, end-begin);
         }
