29f16da889f5fd6398b58381b8527527a35776d5
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-06-26 12:37:30 +0530

    Time: 9 ms (75.23%), Space: 9.7 MB (89.55%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,8 +5,11 @@ public:
         int counter=0, begin=0, end=0, d=0; 
         while(end<s.size()){
             if(map[s[end++]]++>0) counter++; 
-            while(counter>0) if(map[s[begin++]]-->1) counter--;
-            d=max(d, end-begin); //while valid, update d
+            while(counter>0){
+                if(map[s[begin++]]-->1) counter--;
+                
+            }   
+            d=max(d, end-begin);
         }
         return d;
     }
