e013e1d78a7d79a335e45e288de006a720f58bc9
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-07-01 17:42:54 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (45.52%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,28 @@
+class Solution {
+/*
+private:
+    int countPaths(int i, int j, int m, int n){
+        if((i==m-1) && (j==n-1)) return 1;
+        if(i>=m || j>=n) return 0;
+        else return countPaths(i+1,j,m,n) + countPaths(i,j+1,m,n);
+    }
+public:
+    int uniquePaths(int m, int n) {
+        // m = rows, n = cols
+        return countPaths(0,0,m,n);
+    }
+*/
+private:
+    int countPaths(int i, int j, int m, int n, vector<vector<int>> &dp){
+        if((i==m-1) && (j==n-1)) return 1;
+        if(i>=m || j>=n) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        else return dp[i][j]=countPaths(i+1,j,m,n,dp) + countPaths(i,j+1,m,n,dp);
+    }
+public:
+    int uniquePaths(int m, int n) {
+        // m = rows, n = cols
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return countPaths(0,0,m,n,dp);
+    }
+};
