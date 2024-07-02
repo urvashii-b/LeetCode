b71a9bad467fdc8952d2c2e9b5ae40ff40f9d9d7
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-07-02 17:52:10 +0530

    Time: 29 ms (49.15%), Space: 15.8 MB (79.12%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        /*
         int n = nums.size();
         set<vector<int>> st;
         for(int i=0;i<n;i++){
@@ -21,6 +22,37 @@ public:
             }
         }
         vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
+        */
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j!=i+1 && nums[j]==nums[j-1]) continue;
+                int k = j+1;
+                int l = n-1;
+                while(k<l){
+                    long long sum = nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    sum+=nums[l];
+                    if(sum==target){
+                        vector<int> temp = {nums[i],nums[j],nums[k],nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        l--;
+                        while(k<l && nums[l]==nums[l+1]) l--;
+                    }else if(sum>target){
+                        l--;
+                    }else{
+                        k++;
+                    }
+                }
+            }
+        }
         return ans;
     }
 };
