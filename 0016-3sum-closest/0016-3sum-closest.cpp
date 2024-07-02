6579bf32d78c06364a4ddb7a32e9a4ffeb92e74f
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-07-02 18:16:21 +0530

    Time: 11 ms (83.49%), Space: 12.8 MB (26.45%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int mini=INT_MAX, ans = nums[0]+nums[1]+nums[2];
+        int n = nums.size();
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j = i+1, k = n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==target){
+                    return target;
+                }else if(sum>target){
+                    k--;
+                }else{
+                    j++;
+                }
+                int diff = abs(target - sum);
+                if(mini>diff){
+                    ans = sum;
+                    mini=diff;
+                }
+            }
+        }
+        return ans;
+    }
+};
