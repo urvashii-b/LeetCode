1d2bfa88a033f36a5444e9dcc4d506a66270764e
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-07-10 22:57:38 +0530

    Time: 4 ms (18.52%), Space: 7.5 MB (5.94%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     bool checkString(string s) {
-        /**/
+        /*
+        CHECK SORTED
         return is_sorted(s.begin(),s.end());
+        */
+        int n = s.size();
+        for(int i=0;i<n-1;i++){
+            if(s[i+1]<s[i]){
+                return false;
+            }
+        }
+        return true;
     }
 };
