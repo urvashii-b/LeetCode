55ab26c8f57558808ca6271177152754ace629cb
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-07-10 10:53:05 +0530

    Time: 6 ms (18.44%), Space: 14.7 MB (16.05%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,9 +1,41 @@
 class Solution {
+    /*
+private:
+    void permutations(int index, vector<vector<int>>& ans, vector<int>& nums){
+        int n = nums.size()
+        if(index==n){
+            ans.push_back(nums)
+            return;
+        }
+        for(int i=index;i<n;i++){
+            swap(nums[i],nums[index]);
+            permutations(index+1,ans,nums);
+            swap(nums[i],nums[index]); // backtracking
+        }
+    }
+    */
 public:
     void nextPermutation(vector<int>& nums) {
-        // next_permutation(nums.begin(),nums.end()); 
-        int n = nums.size();
-        int index = -1;
+        /* 
+        STL
+        next_permutation(nums.begin(),nums.end()); 
+        
+        
+        ALL PERMUTATIONS
+        vector<vector<int>> ans;
+        permutations(0,ans,nums);
+        sort(ans.begin(),ans.end());
+        auto it = find(ans.begin(),ans.end(),nums);
+        if(it!=ans.end() && next(it)!=ans.end()){
+            nums = *next(it);
+        }else{
+            nums = ans.front();
+        }
+        
+        
+        OPTIMAL
+        */
+        int n = nums.size(), index = -1;
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 index = i;
@@ -13,13 +45,13 @@ public:
         if(index==-1){
             reverse(nums.begin(),nums.end());
         }else{
-            for(int i=n-1;i>index;i--){
+            for(int i=n-1;i>=index;i--){
                 if(nums[i]>nums[index]){
-                    swap(nums[index],nums[i]);
+                    swap(nums[i],nums[index]);
                     break;
                 }
             }
-            reverse(nums.begin()+index+1, nums.end());
-        } 
+            reverse(nums.begin()+index+1,nums.end());
+        }
     }
 };
