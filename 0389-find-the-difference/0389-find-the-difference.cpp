7261ce0b52bc309bada5c0303885cdf5ee18ce89
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-07-12 11:27:48 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (24.03%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {   
+        /*
+        
+        HASHMAP
+        unordered_map<char,int> mpp;
+        for(char &c: t){
+            mpp[c]++;
+        }
+        for(char &x: s){
+            mpp[x]--;
+        }
+        for(auto it: mpp){
+            if(it.second>0){
+                return it.first;
+            }
+        }
+        return t[0]; // will never reach here
+        
+        
+        SUM DIFFERENCE
+        int sum = 0;
+        for(char &c:t){
+            sum+=c;
+        }
+        for(char &c:s){
+            sum-=c;
+        }
+        return (char)sum;
+        
+        
+        CHECK
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         int n = s.size();
@@ -10,5 +40,17 @@ public:
             }
         }
         return t[n];
+        
+        
+        XOR
+        */
+        int XOR = 0;
+        for (char &ch: t)
+            XOR ^= ch;
+        
+        for (char &ch: s)
+            XOR ^=ch;
+        
+        return (char)XOR;
     }
 };
