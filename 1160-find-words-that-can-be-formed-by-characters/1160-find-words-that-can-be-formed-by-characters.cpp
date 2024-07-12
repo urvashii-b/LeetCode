00b2e0163b590c06a17288988c2dedb26db484b0
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-07-12 23:39:21 +0530

    Time: 33 ms (95.96%), Space: 24.2 MB (62.85%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -1,4 +1,5 @@
 class Solution {
+/*
 private:
     bool formed(string s, string chars){
         unordered_map<char, int> mpp;
@@ -15,8 +16,10 @@ private:
         }
         return true;
     }
+*/
 public:
     int countCharacters(vector<string>& words, string chars) {
+        /*
         int n = words.size(), sum = 0;
         for(int i=0;i<n;i++){
             if(formed(words[i], chars)){
@@ -24,5 +27,28 @@ public:
             }
         }
         return sum;
+        */
+        vector<int> charcnt(26,0);
+        for(char &c:chars){
+            charcnt[c-'a']++;
+        }
+        int ans = 0;
+        for(string &s: words){
+            vector<int> wordcnt(26,0);
+            for(char &ch:s){
+                wordcnt[ch-'a']++;
+            }
+            bool flag = true;
+            for(int i=0;i<26;i++){
+                if(wordcnt[i]>charcnt[i]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag==true){
+                ans+= s.size();
+            }
+        }
+        return ans;
     }
 };
