3ae3521ea71c469cbf0caa6719295c8bd2ab62af
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-07-22 23:35:39 +0530

    Time: 2 ms (39.47%), Space: 8.4 MB (8.63%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int reverse(int x) {
-        // O(n) time complexity
-        int rev = 0;
+        int ans = 0;
         while(x!=0){
-            int pop = x%10;
-            x/=10;
-            if(rev>INT_MAX/10 || (rev==INT_MAX && pop>7)) return 0;
-            if(rev<INT_MIN/10 || (rev==INT_MIN && pop<-8)) return 0;
-            rev=rev*10+pop;
-            }
-        return rev;
+            int digit = x%10;
+            x=x/10;
+            if(ans>INT_MAX/10 || (ans==INT_MAX && digit>7)) return 0;   
+            if(ans<INT_MIN/10 || (ans==INT_MIN && digit<-8)) return 0;
+            ans = ans*10+digit;
+        }
+        return ans;
     }
 };
