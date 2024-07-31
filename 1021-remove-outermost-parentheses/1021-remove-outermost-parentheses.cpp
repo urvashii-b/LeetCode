ec4829551ca251f48b822df3389974b24b7ca5d5
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-07-31 11:36:11 +0530

    Time: 5 ms (49.24%), Space: 8.2 MB (29.35%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        /*
-                "(  (   )   (   )   )   (   (   )   )"
-after encounter: 1  2   1   2   1   0   1   2   1   0
-        */
-        string ans = "";
-        int use = 0;
+        string res;
+        int open = 0;
         for(char c:s){
-            if(c=='(' && use++>0){
-                ans+=c;
-            }
-            if(c==')' && use-->1){
-                ans+=c;
-            }   
+            if(c=='(' && open++>0) res+=c;
+            if(c==')' && open-->1) res+=c;
         }
-        return ans;
+        return res;
     }
 };
