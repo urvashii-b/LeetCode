0f9a9bb663f5295e6b0abb91cb1ff286c5f7f040
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-05 23:20:13 +0530

    Time: 5 ms (57.96%), Space: 16.2 MB (44.32%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -9,34 +9,34 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    vis[i][j]=2;
+                    q.push({{i,j},0});  // rotten
+                    vis[i][j]=1;
                 }else{
                     vis[i][j]=0;
                 }
                 if(grid[i][j]==1) cntFresh++;
             }
         }
-        int tm = 0, cnt = 0;
-        int drow[]={0,1,0,-1};
+        int cnt=0, tm = 0;
+        int drow[]={0,-1,0,1};
         int dcol[]={1,0,-1,0};
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
-            q.pop();
             tm = max(tm,t);
+            q.pop();
             for(int i=0;i<4;i++){
                 int nrow = r+drow[i];
                 int ncol = c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]!=2){
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=1 && grid[nrow][ncol]==1){
                     q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol]=2;
+                    vis[nrow][ncol]=1;
                     cnt++;
                 }
             }
-        }
+        } 
         if(cnt!=cntFresh) return -1;
-        return tm;
+        return tm; 
     }
 };
