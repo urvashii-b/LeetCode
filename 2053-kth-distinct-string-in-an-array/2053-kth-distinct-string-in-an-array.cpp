aa8c74707dd880e4d60bf8a52c99ad70bb4e7ab2
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-05 12:46:02 +0530

    Time: 17 ms (51.86%), Space: 18.6 MB (20.16%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
@@ -1,23 +1,36 @@
 class Solution {
-private:
-    vector<string> findDistinct(vector<string> &arr){
-        vector<string> res;
+// private:
+//     vector<string> findDistinct(vector<string> &arr){
+//         vector<string> res;
+//         unordered_map<string,int> freq;
+//         for(const auto& s: arr){
+//             freq[s]++;
+//         }
+//         for(const auto& s: arr){
+//             if(freq[s]==1){
+//                 res.push_back(s);
+//             }
+//         }
+//         return res;
+//     }
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        // vector<string> ans = findDistinct(arr);
+        // if(k>0 && k<=ans.size()){
+        //     return ans[k-1];
+        // }
+        // return "";
         unordered_map<string,int> freq;
         for(const auto& s: arr){
             freq[s]++;
         }
         for(const auto& s: arr){
             if(freq[s]==1){
-                res.push_back(s);
+                k--;
             }
-        }
-        return res;
-    }
-public:
-    string kthDistinct(vector<string>& arr, int k) {
-        vector<string> ans = findDistinct(arr);
-        if(k>0 && k<=ans.size()){
-            return ans[k-1];
+            if(k==0){
+                return s;
+            }
         }
         return "";
     }
