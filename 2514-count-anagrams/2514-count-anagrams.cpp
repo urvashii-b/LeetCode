a8e5f4358d8cbef5749d37c43a5e31c2c05daddd
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-14 15:00:03 +0530

    Time: 78 ms (29.62%), Space: 17.6 MB (64.05%) - LeetHub

--- 2514-count-anagrams/2514-count-anagrams.cpp
+++ 2514-count-anagrams/2514-count-anagrams.cpp
@@ -1,60 +1,48 @@
 class Solution {
 public:
-    const int mod = 1e9+7;
-    vector<long long> fact, invfact;
-    long long modPow(long long base, long long exp, int mod){
-        long long res = 1;
-        while(exp>0){
-            if(exp%2==1){
-                res = (res*base)%mod;
-            }
-            base = (base*base)%mod;
-            exp/=2;
-        }
-        return res;
+    int mod = 1e9+7;
+    int fact[100002];
+    int modmul(int a, int b){
+        return ((long long)(a%mod)*(b%mod))%mod;
     }
-    void getFact(int maxN){
-        fact.resize(maxN+1);
-        invfact.resize(maxN+1);
-        fact[0]=invfact[0]=1;
-        for(int i=1;i<=maxN;i++){
-            fact[i]=(fact[i-1]*i)%mod;
+    int binExpo(int a, int b){
+        if(!b) return 1;
+        int res=binExpo(a,b/2);
+        if(b&1){
+            return modmul(a,modmul(res,res));
+        }else{
+            return modmul(res,res);
         }
-        invfact[maxN]=modPow(fact[maxN],mod-2,mod);
-        for(int i=maxN-1;i>=0;i--){
-            invfact[i]=(invfact[i+1]*(i+1))%mod;
+    }
+    int modmulinv(int a){
+        return binExpo(a,mod-2);
+    }
+    void getFact(){
+        fact[0]=1;
+        for(int i=1;i<=100001;i++){
+            fact[i]=modmul(fact[i-1],i);
         }
+    }
+    int ways(string str){
+        int freq[26] = {0};
+        for(int i = 0; i<str.size(); i++) {
+           freq[str[i] - 'a']++;
+        }
+        int totalWays = fact[str.size()];
+        int factR=1; 
+        for(int i = 0; i<26; i++) {
+            factR=modmul(factR,fact[freq[i]]);
+        }
+        return modmul(totalWays,modmulinv(factR));
     }
     int countAnagrams(string s) {
-        int maxlength = s.length();
-        getFact(maxlength);
-        vector<string> words;
+        getFact();
+        istringstream ss(s);
         string word;
-        for(char c:s){
-            if(c==' '){
-                if(!word.empty()){
-                    words.push_back(word);
-                    word.clear();
-                }
-            }else{
-                word+=c;
-            }
+        int ans = 1;
+        while(ss>>word){
+            ans = modmul(ans,ways(word));
         }
-        if(!word.empty()){
-            words.push_back(word);
-        }
-        long long res = 1;
-        for(string &w: words){
-            unordered_map<char,int> freq;
-            for(char c: w){
-                freq[c]++;
-            }
-            long long permutations = fact[w.length()];
-            for(auto &[ch,count]: freq){
-                permutations = (permutations * invfact[count])%mod;
-            }
-            res= (res*permutations)%mod;
-        }
-        return res;
+        return ans;
     }
 };
