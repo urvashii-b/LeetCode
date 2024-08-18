768d06fb906919398774c0c2e6beddb4b98f72bb
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-18 18:04:28 +0530

    Time: 2 ms (48.05%), Space: 7.7 MB (25.41%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        vector<int> possibilities = {12,23,34,45,56,67,78,89,
-                                    123,234,345,456,567,678,789,
-                                    1234,2345,3456,4567,5678,6789,
-                                    12345,23456,34567,45678,56789,
-                                    123456,234567,345678,456789,
-                                    1234567,2345678,3456789,
-                                    12345678,23456789,
-                                    123456789};
+        // vector<int> possibilities = {12,23,34,45,56,67,78,89,
+        //                             123,234,345,456,567,678,789,
+        //                             1234,2345,3456,4567,5678,6789,
+        //                             12345,23456,34567,45678,56789,
+        //                             123456,234567,345678,456789,
+        //                             1234567,2345678,3456789,
+        //                             12345678,23456789,
+        //                             123456789};
+        // vector<int> res;
+        // int n = possibilities.size();
+        // for(int i=0;i<n;i++){
+        //     if(possibilities[i]<low){
+        //         continue;
+        //     }
+        //     if(possibilities[i]>high){
+        //         break;
+        //     }
+        //     res.push_back(possibilities[i]);
+        // }
+        // return res;
+        queue<int> q;
+        for(int i=1;i<9;i++){
+            q.push(i);
+        }
         vector<int> res;
-        int n = possibilities.size();
-        for(int i=0;i<n;i++){
-            if(possibilities[i]<low){
-                continue;
+        while(!q.empty()){
+            int temp = q.front();
+            q.pop();
+            if(low<=temp && high>=temp){
+                res.push_back(temp);
             }
-            if(possibilities[i]>high){
-                break;
+            int digit = temp%10;
+            if(digit+1<=9){
+                int x = (temp*10+(digit+1));
+                q.push(x);
             }
-            res.push_back(possibilities[i]);
         }
         return res;
     }
