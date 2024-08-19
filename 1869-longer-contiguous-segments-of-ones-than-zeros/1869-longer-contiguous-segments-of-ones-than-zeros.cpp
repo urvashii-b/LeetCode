a1da574ed5288d3fe52ba538ee85b6dca279675d
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-19 08:13:35 +0530

    Time: 3 ms (45.19%), Space: 7.7 MB (19.74%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    bool checkZeroOnes(string s) {
-        int n = s.size(), cnt1 = 0, cnt0 = 0, maxcnt1 = 0, maxcnt0 = 0;
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        // O(n)
+        int n = nums.size(), cnt=0, maxi=0;
         for(int i=0;i<n;i++){
-            if(s[i]=='1'){
-                cnt1++;
-                maxcnt1=max(maxcnt1,cnt1);
-                cnt0=0;
-            }else{
-                cnt1=0;
-                cnt0++;
-                maxcnt0=max(maxcnt0,cnt0);
+            if(nums[i]==1){
+                cnt++;
+                maxi = max(maxi,cnt);
+            }
+            else{
+                cnt = 0;
             }
         }
-        return maxcnt1>maxcnt0;
+        return maxi;
     }
 };
