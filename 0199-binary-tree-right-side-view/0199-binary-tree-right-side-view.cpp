0ea6c8ff9c7d9348ec025a386e884d06efd2dbe8
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-21 15:07:47 +0530

    Time: 2 ms (60.85%), Space: 14.2 MB (70.94%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -10,23 +10,37 @@
  * };
  */
 class Solution {
+private:
+    void preOrder(TreeNode* root, int level, vector<int> &res){
+        if(root==NULL){
+            return;
+        }
+        if(res.size()<level){
+            res.push_back(root->val);
+        }
+        preOrder(root->right,level+1,res);
+        preOrder(root->left,level+1,res);
+    }
 public:
     vector<int> rightSideView(TreeNode* root) {     // level order traversal
-        if(root==NULL) return {};
+        // if(root==NULL) return {};
+        // vector<int> res;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int n = q.size();
+        //     TreeNode* node = NULL;
+        //     while(n--){
+        //         node = q.front();
+        //         q.pop();
+        //         if(node->left!=NULL) q.push(node->left);
+        //         if(node->right!=NULL) q.push(node->right);
+        //     }
+        //     res.push_back(node->val);
+        // }
+        // return res;
         vector<int> res;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            int n = q.size();
-            TreeNode* node = NULL;
-            while(n--){
-                node = q.front();
-                q.pop();
-                if(node->left!=NULL) q.push(node->left);
-                if(node->right!=NULL) q.push(node->right);
-            }
-            res.push_back(node->val);
-        }
+        preOrder(root,1,res);
         return res;
     }
 };
