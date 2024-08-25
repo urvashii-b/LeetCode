650f88228f92d7fff3eb32b4e18f0f32091e7711
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-25 22:46:12 +0530

    Time: 169 ms (85.17%), Space: 119 MB (91.49%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -9,23 +9,58 @@
  * };
  */
 class Solution {
+private:
+    ListNode* reverseLL(ListNode* head){
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* newHead = NULL;
+        while(head!=NULL){
+            ListNode* fwd = head->next;
+            head->next = newHead;
+            newHead = head;
+            head = fwd;
+        }
+        return newHead;
+    }
 public:
     int pairSum(ListNode* head) {
-        vector<int> arr;
-        while(head!=NULL){
-            arr.push_back(head->val);
-            head=head->next;
+        // vector<int> arr;
+        // ListNode* head2 = head;
+        // while(head2!=NULL){
+        //     arr.push_back(head2->val);
+        //     head2=head2->next;
+        // }
+        // int sum = 0, maxii = 0, n = arr.size();
+        // int i=0, j=n-1;
+        // while(i<j){
+        //     sum = arr[i]+arr[j];
+        //     if(sum>maxii){
+        //         maxii = sum;
+        //     }
+        //     i++;
+        //     j--;
+        // }
+        // return maxii;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prevSlow = NULL;
+        while(fast!=NULL && fast->next!=NULL){
+            prevSlow = slow;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int sum = 0, maxii = 0, n = arr.size();
-        int i=0, j=n-1;
-        while(i<j){
-            sum = arr[i]+arr[j];
-            if(sum>maxii){
-                maxii = sum;
+        prevSlow->next = NULL;
+        ListNode* L2 = reverseLL(slow);
+        int sum = 0, maxi = 0;
+        while(L2!=NULL){
+            sum = L2->val + head->val;
+            if(sum>maxi){
+                maxi=sum;
             }
-            i++;
-            j--;
+            head=head->next;
+            L2=L2->next;
         }
-        return maxii;
+        return maxi;
     }
 };
