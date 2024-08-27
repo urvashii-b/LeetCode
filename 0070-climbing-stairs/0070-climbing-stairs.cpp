978360a75cf903a101bd0dadc488a4e7aec9de37
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-27 19:40:37 +0530

    Time: 2 ms (49.38%), Space: 7.4 MB (77.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int climbStairs(int n) {
         if(n==0 || n==1) return 1;
-        vector<int> dp(n+1);
-        dp[0]=dp[1]=1;
+        int c;
+        int a = 1, b = 1;
         for(int i=2;i<=n;i++){
-           dp[i]=dp[i-1]+dp[i-2]; 
+            c = a+b;
+            a = b;
+            b= c;
         }
-        return dp[n];
+        return c;
     }
 };
