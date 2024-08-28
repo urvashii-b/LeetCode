22191bc11e6f5b6f7167e9e8d1061c9259452df8
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-28 16:21:15 +0530

    Time: 0 ms (100.00%), Space: 9 MB (9.09%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,7 +4,7 @@ public:
         int n = matrix.size();
         // transpose
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
+            for(int j=i;j<n;j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
