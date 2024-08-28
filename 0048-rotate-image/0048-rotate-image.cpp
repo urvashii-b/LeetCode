42f2ead67da2eecbdfb3c332514b899c352cc0cf
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-28 16:17:21 +0530

    Time: 0 ms (100.00%), Space: 9 MB (47.68%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         // transpose
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
