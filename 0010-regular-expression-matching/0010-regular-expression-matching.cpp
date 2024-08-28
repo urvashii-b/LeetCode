b534101e1bf0018605c3aa93d11df1bd68e48529
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-28 11:10:08 +0530

    Time: 0 ms (100.00%), Space: 9 MB (44.94%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -2,6 +2,7 @@ class Solution {
 private:
     bool solve(string &text, string &pattern, int i, int j, vector<vector<int>> &dp){
         if(j<0) return i<0;   // if path is done, and the string is also done 
+        if(j<0) return false;
         if(i<0 && j>=0){ //string exhausted but matchings left
             if(pattern[j]=='*'){
                 return solve(text,pattern,i,j-2,dp);
@@ -13,7 +14,7 @@ private:
             return dp[i][j]=solve(text,pattern,i-1,j-1,dp);
         }
         if(pattern[j]=='*'){
-            if(solve(text,pattern,i,j-2,dp)) return true;
+            if(solve(text,pattern,i,j-2,dp)) return dp[i][j]=true;
             if(pattern[j-1]=='.' || pattern[j-1]==text[i]){
                 return dp[i][j]=solve(text,pattern,i-1,j,dp);
             }
@@ -24,7 +25,7 @@ public:
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
-        vector<vector<int>> dp(n,vector<int> (m,-1));
+        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
         return solve(s,p,n-1,m-1,dp);
     }
 };
