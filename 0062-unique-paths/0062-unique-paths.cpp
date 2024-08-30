82b256f8d958a68806324427cac2e5b3e3667e9d
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-30 14:52:59 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (18.40%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -20,12 +20,11 @@ private:
         if((i==m-1) && (j==n-1)) return 1;
         if(i>=m || j>=n) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        else return dp[i][j]=countPaths(i+1,j,m,n,dp) + countPaths(i,j+1,m,n,dp);
+        return dp[i][j] = countPaths(i+1,j,m,n,dp) + countPaths(i,j+1,m,n,dp);
     }
 public:
     int uniquePaths(int m, int n) {
-        // m = rows, n = cols
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m, vector<int> (n,-1));
         return countPaths(0,0,m,n,dp);
     }
 };
