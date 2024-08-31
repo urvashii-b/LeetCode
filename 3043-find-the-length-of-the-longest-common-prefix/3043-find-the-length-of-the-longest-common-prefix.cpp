7b791509e4f06b84038cd0351b94d5ec34c29baf
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-31 17:06:09 +0530

    Time: 354 ms (59.80%), Space: 123.2 MB (99.23%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,45 +1,31 @@
 class Solution {
+private:
+    int findCommonPrefixLength(string &s1, string &s2){
+        int i=0;
+        while(i<s1.length() && i<s2.length() && s1[i]==s2[i]){
+            i++;
+        }
+        return i;
+    }
 public:
-    class Node{
-        public:
-            Node* digits[10];
-    };
-    
-    void add(Node* head, int num){
-        string s = to_string(num);
-        Node* temp = head;
-        for(auto c: s){
-            if(!temp->digits[c-'0']){
-                temp->digits[c-'0'] = new Node();
-            }
-            temp = temp->digits[c-'0'];
-        }
-    }
-    
-    int check(Node* head, int num){
-        Node* temp = head;
-        string t = to_string(num);
-        int len = 0;
-        for(auto c:t){
-            if(temp->digits[c-'0']){
-                len++;
-            }else{
-                break;
-            }
-            temp = temp->digits[c-'0'];  
-        }
-        return len;
-    }
-    
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        int ans = 0;
-        Node* head = new Node();
-        for(auto num:arr1){
-            add(head,num);
+        vector<string> str1(arr1.size());
+        vector<string> str2(arr2.size());
+        transform(arr1.begin(), arr1.end(), str1.begin(), [](int num) { return to_string(num); });
+        transform(arr2.begin(), arr2.end(), str2.begin(), [](int num) { return to_string(num); });
+        sort(str1.begin(), str1.end());
+        sort(str2.begin(), str2.end());
+        int maxLength = 0;
+        int i = 0, j = 0;
+        while (i < str1.size() && j < str2.size()) {
+            int commonLength = findCommonPrefixLength(str1[i], str2[j]);
+            maxLength = max(maxLength, commonLength);
+            if (str1[i] < str2[j]) {
+                i++;
+            } else {
+                j++;
+            }
         }
-        for(auto num:arr2){
-            ans = max(ans,check(head,num));
-        }
-        return ans;
+        return maxLength;
     }
 };
