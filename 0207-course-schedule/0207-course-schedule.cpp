020447549742863062d94c9388f2b2250d441f22
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-31 09:25:59 +0530

    Time: 11 ms (92.13%), Space: 17.6 MB (49.37%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,39 @@
 class Solution {
 private:
-    bool hasCycle(int i, vector<int> &vis, unordered_map<int,vector<int>> &adj){
-        vis[i]=1;
-        for(int neighbor: adj[i]){
-            if(vis[neighbor]==1){
-                return true;
-            }
-            if(vis[neighbor]==0 && hasCycle(neighbor,vis,adj)){
-                return true;
+    bool topologicalSort(int n, unordered_map<int,vector<int>> &adj, vector<int> &inDegree){
+        queue<int> q;
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(inDegree[i]==0){
+                q.push(i);
+                cnt++;
             }
         }
-        vis[i]=2;
-        return false;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(auto& v: adj[u]){
+                inDegree[v]--;
+                
+                if(inDegree[v]==0){
+                    q.push(v);
+                    cnt++;
+                }
+            }
+        }
+        return (cnt==n);
     }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>> adj; //graph
-        for(vector<int> &vec: prerequisites){
-            int u = vec[0], v = vec[1];
-            adj[v].push_back(u);    // if v then u
+        // if topological sort - no cycle - courses can be scheduled
+        unordered_map<int,vector<int>> adj;
+        vector<int> inDegree(numCourses,0);
+        for(auto& vec: prerequisites){
+            int u = vec[0];
+            int v = vec[1];
+            adj[v].push_back(u);
+            inDegree[u]++;
         }
-        vector<int> vis(numCourses,0);
-        for(int i=0;i<numCourses;i++){
-            if(vis[i]==0 && hasCycle(i,vis,adj)){
-                return false;
-            }
-        }
-        return true;
+        return topologicalSort(numCourses, adj, inDegree);
     }
 };
