584f13d7b9e5230f7e48d30aef1ea40fc9a48af9
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-31 09:14:24 +0530

    Time: 8 ms (99.71%), Space: 19.1 MB (53.89%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -31,14 +31,48 @@
 //         return provinces;
 //     }
 // };
+
+// class Solution {
+// private:
+//     void dfs(vector<vector<int>> &isConnected, int u, vector<bool> &vis){
+//         vis[u]=true;
+//         int n = isConnected.size();
+//         for(int v = 0; v<n; v++){
+//             if(!vis[v] && isConnected[u][v]==1){
+//                 dfs(isConnected,v,vis);
+//             }
+//         }
+//     }
+// public:
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n = isConnected.size();
+//         vector<bool> vis(n, false);
+//         int provinces = 0;
+//         for(int i=0;i<n;i++){
+//             if(!vis[i]){
+//                 dfs(isConnected,i,vis);
+//                 provinces++;
+//             }
+//         }
+//         return provinces;
+//     }
+// };
+
 class Solution {
 private:
-    void dfs(vector<vector<int>> &isConnected, int u, vector<bool> &vis){
-        vis[u]=true;
+    void bfs(vector<vector<int>> &isConnected, int u, vector<bool> &vis){
         int n = isConnected.size();
-        for(int v = 0; v<n; v++){
-            if(!vis[v] && isConnected[u][v]==1){
-                dfs(isConnected,v,vis);
+        queue<int> q;
+        q.push(u);
+        vis[u]=true;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(int v=0; v<n; v++){
+                if(!vis[v] && isConnected[u][v]==1){
+                    q.push(v);
+                    vis[v]=true;
+                }
             }
         }
     }
@@ -49,7 +83,7 @@ public:
         int provinces = 0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                dfs(isConnected,i,vis);
+                bfs(isConnected,i,vis);
                 provinces++;
             }
         }
