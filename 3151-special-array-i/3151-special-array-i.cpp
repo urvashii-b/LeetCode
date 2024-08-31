11f4e06b836165bff47fd6582edf265401af44e7
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-31 13:52:33 +0530

    Time: 7 ms (50.95%), Space: 27.1 MB (35.34%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.cpp
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        for(int i=1;i<nums.size();i++){
+        int n = nums.size();
+        if(n==1) return true;
+        for(int i=1;i<n;i++){
             if((nums[i-1]&1)^(nums[i]&1)==0){ // 0-0 or 1-1 gives 0 (bitwise or)
                 return false;
             }
