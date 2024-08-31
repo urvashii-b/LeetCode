ca00f1d9e3314d42f8ad7940348fb0cb32397ce0
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-08-31 09:06:26 +0530

    Time: 25 ms (28.82%), Space: 18.3 MB (78.94%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,10 +1,44 @@
+// class Solution {
+// private:
+//     void dfs(unordered_map<int,vector<int>> &adj, int u, vector<bool> &vis){
+//         vis[u]=true;
+//         for(int& v: adj[u]){
+//             if(!vis[v]){
+//                 dfs(adj,v,vis);
+//             }
+//         }
+//     }
+// public:
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n = isConnected.size();
+//         vector<bool> vis(n, false);
+//         unordered_map<int,vector<int>> adj;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(isConnected[i][j]==1){
+//                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
+//                 }
+//             }
+//         }
+//         int provinces = 0;
+//         for(int i=0;i<n;i++){
+//             if(!vis[i]){
+//                 dfs(adj,i,vis);
+//                 provinces++;
+//             }
+//         }
+//         return provinces;
+//     }
+// };
 class Solution {
 private:
-    void dfs(unordered_map<int,vector<int>> &adj, int u, vector<bool> &vis){
+    void dfs(vector<vector<int>> &isConnected, int u, vector<bool> &vis){
         vis[u]=true;
-        for(int& v: adj[u]){
-            if(!vis[v]){
-                dfs(adj,v,vis);
+        int n = isConnected.size();
+        for(int v = 0; v<n; v++){
+            if(!vis[v] && isConnected[u][v]==1){
+                dfs(isConnected,v,vis);
             }
         }
     }
@@ -12,19 +46,10 @@ public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
         vector<bool> vis(n, false);
-        unordered_map<int,vector<int>> adj;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
         int provinces = 0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                dfs(adj,i,vis);
+                dfs(isConnected,i,vis);
                 provinces++;
             }
         }
