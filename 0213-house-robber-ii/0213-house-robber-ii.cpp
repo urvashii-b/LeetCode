d4f55b9da46eba029067ab795bae909d029e86db
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-09-01 14:57:02 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (11.82%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -26,26 +26,54 @@
 //     }
 // };
 
+// class Solution {
+// private:
+//     int solveTabulation(int end, vector<int>& nums, int start){
+//         vector<int> dp(nums.size(), 0);
+//         dp[start] = nums[start];
+//         for(int i=start+1; i<=end; i++){
+//             int pick = nums[i] + (i > start+1? dp[i-2]: 0);
+//             int notpick = 0 + dp[i-1];
+//             dp[i]=max(pick,notpick);
+//         }
+//         return dp[end];
+//     }
+// public:
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n==1) return nums[0];
+        
+//         int first = solveTabulation(n-1,nums,1);
+//         int second = solveTabulation(n-2,nums,0);
+        
+//         return max(first,second);
+//     }
+// };
+
 class Solution {
 private:
-    int solveTabulation(int end, vector<int>& nums, int start){
-        vector<int> dp(nums.size(), 0);
-        dp[start] = nums[start];
-        for(int i=start+1; i<=end; i++){
-            int pick = nums[i] + (i > start+1? dp[i-2]: 0);
-            int notpick = 0 + dp[i-1];
-            dp[i]=max(pick,notpick);
+    int solve(vector<int>& nums){
+        int n = nums.size();
+        int prev2 = 0;
+        int prev = nums[0];
+        for(int i=1; i<n; i++){
+            int pick = nums[i] + (i>1? prev2: 0);
+            int notpick = 0 + prev;
+            int cur = max(pick,notpick);
+            prev2 = prev;
+            prev = cur;
         }
-        return dp[end];
+        return prev;
     }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return nums[0];
-        
-        int first = solveTabulation(n-1,nums,1);
-        int second = solveTabulation(n-2,nums,0);
-        
-        return max(first,second);
+        vector<int> temp1(n), temp2(n);
+        for(int i=0; i<n; i++){
+            if(i!=0) temp1.push_back(nums[i]);
+            if(i!=n-1) temp2.push_back(nums[i]);
+        }
+        return max(solve(temp1), solve(temp2));
     }
 };
