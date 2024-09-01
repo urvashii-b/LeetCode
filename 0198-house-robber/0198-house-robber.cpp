f8749ca42a7dec414684df6aeb112a4a5a3051a0
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-09-01 13:24:24 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (54.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,41 @@
-class Solution {
-private:
-    int solve(int index, vector<int> &nums, vector<int> &dp){
-        if(index==0) return nums[0];
-        if(index<0) return 0;
-        if(dp[index]!=-1) return dp[index];
-        // pick
-        int pick = nums[index]+solve(index-2, nums, dp);
-        // not pick
-        int notpick = 0 + solve(index-1, nums, dp);
+// class Solution {
+// private:
+//     int solve(int index, vector<int> &nums, vector<int> &dp){
+//         if(index==0) return nums[0];
+//         if(index<0) return 0;
+//         if(dp[index]!=-1) return dp[index];
+//         // pick
+//         int pick = nums[index]+solve(index-2, nums, dp);
+//         // not pick
+//         int notpick = 0 + solve(index-1, nums, dp);
         
-        return dp[index] = max(pick, notpick);
-    }
+//         return dp[index] = max(pick, notpick);
+//     }
+// public:
+//     int rob(vector<int>& nums) {
+//         // no adjacent houses
+//         int n = nums.size();
+//         vector<int> dp(n+1, -1);
+//         return solve(n-1, nums, dp);
+//     }
+// };
+
+class Solution {
 public:
     int rob(vector<int>& nums) {
         // no adjacent houses
         int n = nums.size();
         vector<int> dp(n+1, -1);
-        return solve(n-1, nums, dp);
+        dp[0] = nums[0];
+        int neg = 0;
+        for(int i=1; i<n; i++){
+            int pick = nums[i];
+            if(i>1){
+                pick += dp[i-2];
+            }
+            int notpick = 0 + dp[i-1];
+            dp[i] = max(pick, notpick);
+        }
+        return dp[n-1];
     }
 };
