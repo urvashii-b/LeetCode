331473c5827ecade3b701dd1c94093de78338c4d
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-09-01 17:15:58 +0530

    Time: 37 ms (29.30%), Space: 19.7 MB (39.94%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,32 +1,51 @@
 class Solution {
+private:
+    int maximumArea(vector<int>& heights){
+        int n = heights.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        stack<int> st;
+        
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                st.pop();
+            }
+            if(st.empty()) left[i]=0;
+            else left[i]=st.top()+1;
+            st.push(i);
+        }
+        
+        while(!st.empty()) st.pop();
+        
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                st.pop();
+            }
+            if(st.empty()) right[i]=n-1;
+            else right[i]=st.top()-1;
+            st.push(i);
+        }
+        
+        int maxArea = 0;
+        for(int i=0; i<n; i++){
+            maxArea = max(maxArea, heights[i]*(right[i]-left[i]+1));
+        }
+        return maxArea;
+    }
+
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty())
-            return 0;
-
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<int> heights(cols + 1, 0); // Include an extra element for easier calculation
+        int n = matrix.size(), m = matrix[0].size();
         int maxArea = 0;
-
-        for (const auto& row : matrix) {
-            for (int i = 0; i < cols; i++) {
-                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
+        vector<int> height(m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1') height[j]++;
+                else height[j]=0;
             }
-
-            // Calculate max area using stack-based method
-            stack<int> stk;
-            for (int i = 0; i < heights.size(); i++) {
-                while (!stk.empty() && heights[i] < heights[stk.top()]) {
-                    int h = heights[stk.top()];
-                    stk.pop();
-                    int w = stk.empty() ? i : i - stk.top() - 1;
-                    maxArea = max(maxArea, h * w);
-                }
-                stk.push(i);
-            }
+            int area = maximumArea(height);
+            maxArea = max(maxArea,area);
         }
-
         return maxArea;
     }
 };
