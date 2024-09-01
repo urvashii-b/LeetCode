32b7e5e8aa9b58cdf60063d8eae4fdb529e75f30
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-09-01 18:14:40 +0530

    Time: 3 ms (84.25%), Space: 16.1 MB (81.44%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
         int n = grid.size(), m = grid[0].size();
-        queue<pair<pair<int,int>,int>> q;
         int vis[n][m];
         int cntFresh = 0;
+        queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j},0});  // rotten
-                    vis[i][j]=1;
+                    q.push({{i,j},0});
+                    vis[i][j]=2;
                 }else{
                     vis[i][j]=0;
                 }
-                if(grid[i][j]==1) cntFresh++;
+                if(grid[i][j]==1){
+                    cntFresh++;
+                }
             }
         }
-        int cnt=0, tm = 0;
-        int drow[]={0,-1,0,1};
-        int dcol[]={1,0,-1,0};
+        int tm = 0;
+        int delRow[]={-1,0,1,0};
+        int delCol[]={0,-1,0,1};
+        int cnt = 0;
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
-            tm = max(tm,t);
             q.pop();
+            tm = max(tm,t);
             for(int i=0;i<4;i++){
-                int nrow = r+drow[i];
-                int ncol = c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=1 && grid[nrow][ncol]==1){
+                int nrow = r+delRow[i];
+                int ncol = c+delCol[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
                     q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol]=1;
+                    vis[nrow][ncol]=2;
                     cnt++;
                 }
             }
-        } 
-        if(cnt!=cntFresh) return -1;
-        return tm; 
+        }
+        if(cntFresh!=cnt) return -1;
+        return tm;
     }
 };
