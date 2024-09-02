23598172142606b1fb6a6d32a5260f1ff6d38cbf
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-09-02 07:42:47 +0530

    Time: 40 ms (86.82%), Space: 48.4 MB (71.57%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -2,16 +2,16 @@ struct Node{
     Node* links[26];
     bool flag = false;
     
-    bool hasKey(char ch){
-        return (links[ch-'a']!=NULL);
+    bool containsKey(char c){
+        return links[c-'a']!=NULL;
     }
     
-    void put(char ch, Node* node){
-        links[ch-'a']=node;
+    void put(char c, Node* node){
+        links[c -'a'] = node;
     }
     
-    Node* get(char ch){
-        return links[ch-'a'];
+    Node* get(char c){
+        return links[c-'a'];
     }
     
     void setEnd(){
@@ -21,48 +21,48 @@ struct Node{
     bool isEnd(){
         return flag;
     }
-    
 };
+
 class Trie {
 private:
     Node* root;
 public:
     Trie(){
-            root = new Node();
-        }
+        root = new Node();
+    }
 
-        void insert(string word){
-            Node* node = root;
-            for(int i=0;i<word.length();i++){
-                if(!node->hasKey(word[i])){
-                    node->put(word[i],new Node());
-                }
-                node = node->get(word[i]);
+    void insert(string word){
+        Node* temp = root;
+        for(int i=0; i<word.length(); i++){
+            if(!temp->containsKey(word[i])){
+                temp->put(word[i], new Node());
             }
-            node->setEnd();
+            temp=temp->get(word[i]);
         }
+        temp->setEnd();
+    }
 
-        bool search(string word){
-            Node* node = root;
-            for(int i=0;i<word.length();i++){
-                if(!node->hasKey(word[i])){
-                    return false;
-                }
-                node = node->get(word[i]);
+    bool search(string word){
+        Node* temp = root;
+        for(int i=0; i<word.length(); i++){
+            if(!temp->containsKey(word[i])){
+                return false;
             }
-            return node->isEnd();
+            temp = temp->get(word[i]);
         }
+        return temp->isEnd();
+    }
 
-        bool startsWith(string prefix){
-            Node* node = root;
-            for(int i=0;i<prefix.length();i++){
-                if(!node->hasKey(prefix[i])){
-                    return false;
-                }
-                node = node->get(prefix[i]);
+    bool startsWith(string prefix){
+        Node* temp = root;
+        for(int i=0; i<prefix.length(); i++){
+            if(!temp->containsKey(prefix[i])){
+                return false;
             }
-            return true;
+            temp = temp->get(prefix[i]);
         }
+        return true;
+    }
 };
 
 /**
