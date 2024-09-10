5966d6fb5f61c82ce45239594863675353657a1a
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-09-10 18:20:54 +0530

    Time: 4 ms (95.65%), Space: 17.3 MB (6.16%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,104 +1,58 @@
 class Solution {
-    /*
 private:
-    void markRow(int i, vector<vector<int>>& matrix, int n, int m){
-        for(int j=0;j<m;j++){
+    void markRow(int i, int j, int n, int m, vector<vector<int>>& matrix){
+        for(int j=0; j<m; j++){
             if(matrix[i][j]!=0){
-                matrix[i][j]=-1;
+                matrix[i][j] = -1;
             }
         }
     }
-    void markCol(int j, vector<vector<int>>& matrix, int n, int m){
-        for(int i=0;i<n;i++){
+    
+    void markCol(int i, int j, int n, int m, vector<vector<int>>& matrix){
+        for(int i=0; i<n; i++){
             if(matrix[i][j]!=0){
-                matrix[i][j]=-1;
+                matrix[i][j] = -1;
             }
         }
     }
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        /*
+        Brute force
+        
+        int n = matrix.size(), m = matrix[0].size();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(matrix[i][j]==0){
-                    markRow(i, matrix, n, m);
-                    markCol(j, matrix, n, m);
+                    markRow(i,j,n,m,matrix);
+                    markCol(i,j,n,m,matrix);
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(matrix[i][j]==-1){
                     matrix[i][j]=0;
                 }
             }
         }
-    }    
-};
-
-
-
-public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int row[200]={0};
-        int col[200]={0};
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        */
+        int n = matrix.size(), m = matrix[0].size();
+        int row[201]={0};
+        int col[201]={0};
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(matrix[i][j]==0){
                     row[i]=1;
                     col[j]=1;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(row[i]==1 || col[j]==1){
                     matrix[i][j]=0;
                 }
-            }
-        }
-    }    
-};
-    */
-public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        // row---> matrix[i][0] & col---> matrix[0][j]
-        int col0 = 1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    if(j!=0){
-                        matrix[0][j]=0;
-                    }
-                    else{
-                        col0 = 0;
-                    }
-                }
-            }
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][j]!=0){
-                    if(matrix[i][0]==0 || matrix[0][j]==0){
-                        matrix[i][j]=0;
-                    }
-                }
-            }
-        }
-        if(matrix[0][0]==0){
-            for(int j=0;j<m;j++){
-                matrix[0][j]=0;
-            }
-        }
-        if(col0==0){
-            for(int i=0;i<n;i++){
-                matrix[i][0]=0;
             }
         }
     }    
