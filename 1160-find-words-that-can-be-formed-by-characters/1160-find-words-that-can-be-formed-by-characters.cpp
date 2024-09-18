ca1b19879502d0857ba5305a8969da4d7dcd7b78
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-09-18 10:33:27 +0530

    Time: 46 ms (73.30%), Space: 24.2 MB (74.05%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -1,54 +1,32 @@
 class Solution {
-/*
-private:
-    bool formed(string s, string chars){
-        unordered_map<char, int> mpp;
-        for(char &c: s){
-            mpp[c]++;
-        }
-        for(char &c: chars){
-            mpp[c]--;
-        }
-        for(auto it: mpp){
-            if(it.second>0){
-                return false;
-            }
-        }
-        return true;
-    }
-*/
 public:
     int countCharacters(vector<string>& words, string chars) {
         /*
-        int n = words.size(), sum = 0;
-        for(int i=0;i<n;i++){
-            if(formed(words[i], chars)){
-                sum+=words[i].length();
-            }
-        }
-        return sum;
+        
+        lowercase English letters - use vector of size 26 OR map
+        
         */
-        vector<int> charcnt(26,0);
-        for(char &c:chars){
-            charcnt[c-'a']++;
+        vector<int> charCnt(26,0);
+        for(char& c: chars){
+            charCnt[c-'a']++;
         }
-        int ans = 0;
-        for(string &s: words){
-            vector<int> wordcnt(26,0);
-            for(char &ch:s){
-                wordcnt[ch-'a']++;
+        int res = 0;
+        for(string& word: words){
+            vector<int> wordCnt(26,0);
+            for(char& c: word){
+                wordCnt[c-'a']++;
             }
-            bool flag = true;
-            for(int i=0;i<26;i++){
-                if(wordcnt[i]>charcnt[i]){
-                    flag = false;
+            bool ok = true;
+            for(int i=0; i<26; i++){
+                if(wordCnt[i]>charCnt[i]){
+                    ok = false;
                     break;
                 }
             }
-            if(flag==true){
-                ans+= s.size();
+            if(ok==true){
+                res+=word.length();
             }
         }
-        return ans;
+        return res;
     }
 };
