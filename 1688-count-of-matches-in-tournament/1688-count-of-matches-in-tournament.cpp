dd79d14c1d2a5ca49834069a6db1e84eeb0b8d65
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-09-18 10:41:59 +0530

    Time: 3 ms (43.98%), Space: 7.5 MB (82.58%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int matches = 0;
+        while(n!=1){
+            if(n%2==0){ //even
+                matches += (n/2);
+                n = (n/2);
+            }else{
+                matches += ((n-1)/2);
+                n = ((n-1)/2)+1;
+            }
+        }
+        return matches;
+    }
+};
