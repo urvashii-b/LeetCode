253b7fad6c016dbdfb94d2712827ebc452fb831a
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-09-18 10:46:29 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (54.54%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
+        /* 
+        log(n) time complexity
         int matches = 0;
         while(n!=1){
             if(n%2==0){ //even
@@ -12,5 +14,12 @@ public:
             }
         }
         return matches;
+        
+        logic: if we have n teams, we will have 1 winner and n-1 will be eliminated 
+        for 1 match we would have 2 teams and 1 would be eliminated
+        so, no. of matches played = no. of people eliminated
+        n-1 matches will be played
+        */
+        return n-1;
     }
 };
