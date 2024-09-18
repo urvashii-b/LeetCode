5fd4960908c5dbb778a354040b5ccfbe5ed6d0bb
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-09-18 09:54:58 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (85.08%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,55 +2,52 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {   
         /*
-        
-        HASHMAP
+        n = t.length() and m = s.length()
+        1. hashmap - extra o(n) space, linear time
         unordered_map<char,int> mpp;
         for(char &c: t){
             mpp[c]++;
         }
-        for(char &x: s){
-            mpp[x]--;
+        for(char &c: s){
+            mpp[c]--;
         }
-        for(auto it: mpp){
+        for(auto& it: mpp){
             if(it.second>0){
                 return it.first;
             }
         }
-        return t[0]; // will never reach here
+        return t[0]; // never reaches here
         
-        
-        SUM DIFFERENCE
+        2. sum difference - linear time, constant space
         int sum = 0;
-        for(char &c:t){
+        for(char &c: t){
             sum+=c;
         }
-        for(char &c:s){
+        for(char &c: s){
             sum-=c;
         }
         return (char)sum;
         
-        
-        CHECK
+        3. sorting
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        int n = s.size();
-        for(int i=0;i<n;i++){
+        int n = s.length();
+        for(int i=0; i<n; i++){
             if(s[i]!=t[i]){
                 return t[i];
             }
         }
         return t[n];
         
-        
-        XOR
+        4. xor - linear time, constant space
         */
         int XOR = 0;
-        for (char &ch: t)
-            XOR ^= ch;
-        
-        for (char &ch: s)
-            XOR ^=ch;
-        
+        for(char& c:s){
+            XOR^=c;
+        }
+        for(char& c:t){
+            XOR^=c;
+        }
         return (char)XOR;
     }
 };
