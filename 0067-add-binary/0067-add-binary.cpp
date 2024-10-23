737c161aa7701c565a456997daac26913efd666a
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-10-23 16:21:18 +0530

    Time: 2 ms (60.19%), Space: 10.2 MB (33.51%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string ans="";
-        int carry = 0, i = a.size()-1, j = b.size()-1;
-        while (i >= 0 || j >= 0 || carry) {
+        string ans = "";
+        int carry = 0;
+        int i = a.length()-1, j = b.length()-1;
+        while(i>=0 || j>=0 || carry){
             int sum = carry;
-            if(i>=0){
-                sum+=a[i--]-'0';
-            }
-            if(j>=0){
-                sum+=b[j--]-'0';
-            }
+            if(i>=0) sum+=a[i--]-'0';
+            if(j>=0) sum+=b[j--]-'0';
+            ans+= to_string(sum%2);
             carry = sum/2;
-            sum=sum%2;
-            ans=to_string(sum)+ans;
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
